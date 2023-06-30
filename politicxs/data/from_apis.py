dffdb6dd221be8023108016a0140423b8b09e3e4
Pano Valdez <marianovaldez92@protonmail.com>
2023-06-30 15:32:19 -0300

    agrego variables

--- politicxs/data/from_apis.py
+++ politicxs/data/from_apis.py
@@ -7,3 +7,5 @@ WIKIDATA_PAGES = pd.read_csv('data/wikidata_links_politicxs_2023.csv')
 WIKIPEDIA_VALUES = pd.read_csv('data/wikipedia_values_politicxs_2023.csv')
 
 WIKIDATA_VALUES = pd.read_csv('data/wikidata_values_politicxs_2023.csv')
+
+GOOGLE_VALUES = pd.read_csv('data/google_values_politicxs_2023.csv')
