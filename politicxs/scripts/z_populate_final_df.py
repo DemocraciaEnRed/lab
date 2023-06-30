45ab451a07d5b57287fa473889596cc605554bc5
Pano Valdez <marianovaldez92@protonmail.com>
2023-06-30 15:32:29 -0300

    populate final

--- politicxs/scripts/z_populate_final_df.py
+++ politicxs/scripts/z_populate_final_df.py
@@ -17,18 +17,18 @@ CANDIDATURAS = pd.merge(df_input, PUESTOS_AREAS, left_on=['state', 'role_type'],
 CANDIDATURAS['area'] = CANDIDATURAS['role_type_area_id'].fillna(0).astype(int)
 # Agrego membership_type
 CANDIDATURAS['membership_type'] = "campaign_politician"
-# Agrego páginas de wikipedia
-CANDIDATURAS = pd.merge(CANDIDATURAS, WIKIPEDIA_PAGES, on='person_id', how='inner')
-# Agrego páginas de wikidata
-CANDIDATURAS = pd.merge(CANDIDATURAS, WIKIDATA_PAGES, on='person_id', how='inner')
 # Agrego data de wikipedia
 CANDIDATURAS = pd.merge(CANDIDATURAS, WIKIPEDIA_VALUES, on='person_id', how='inner')
 # Agrego data de wikidata
 CANDIDATURAS = pd.merge(CANDIDATURAS, WIKIDATA_VALUES, on='person_id', how='inner')
+# Agrego data de google
+CANDIDATURAS = pd.merge(CANDIDATURAS, GOOGLE_VALUES, on='person_id', how='inner')
 
-
+# CANDIDATURAS['date_birth'].fillna(CANDIDATURAS['WIKIDATA_FECHA_NACIMIENTO'], inplace=True)
+# CANDIDATURAS['place_of_birth'].fillna(CANDIDATURAS['WIKIDATA_LUGAR_NACIMIENTO'], inplace=True)
+# CANDIDATURAS['gender'].fillna(CANDIDATURAS['WIKIDATA_GENERO'], inplace=True)
 
 # Elimino columnas innecesarias
-CANDIDATURAS = CANDIDATURAS[df_input.columns]
+# CANDIDATURAS = CANDIDATURAS[df_input.columns]
 
-CANDIDATURAS.to_csv('outputs/elecciones_2023.csv')
+CANDIDATURAS.to_csv('outputs/elecciones_2023.csv', index=False)
