359ce29173cbd4839fb6b14f1489a950a4807505
Pano Valdez <marianovaldez92@protonmail.com>
2023-06-30 15:32:37 -0300

    runs

--- politicxs/run.py
+++ politicxs/run.py
@@ -1,8 +1,9 @@
 from scripts import (
     # a_get_links_wikidata,
-    b_get_links_wikipedia,
+    # b_get_links_wikipedia,
     # c_selenium_check_pages,
-    d_get_values_wikidata,
-    e_get_values_wikipedia,
-    # f_get_from_google_search
+    # d_get_values_wikidata,
+    # e_get_values_wikipedia,
+    # f_get_from_google_search,
+    z_populate_final_df
 )
